cefb9744158c38f807ff831d0f032b32a5f941c0
Tony Roberts <tony@pyxll.com>
2015-04-24 16:09:46 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,19 @@ import a large number of modules.
 
 See PEP 302 (http://www.python.org/dev/peps/pep-0302/) for more info.
 """
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="cachedimporter",
-    author="Renshaw Bay",
     description="Python import hook that caches directory listings",
+    packages=find_packages(),
+    py_modules=["cachedimporter"],
+    version="0.1.1",
+    test_suite="nose.collector",
+    tests_require=["nose>=1.2.1"],
+    author="Renshaw Bay",
+    author_email="technology@renshawbay.com",
     license="MIT",
     url="https://github.com/renshawbay/cachedimporter",
-    version="0.1",
-    py_modules=["test_import"],
-    test_suite="tests"
+    classifiers=["License :: OSI Approved :: MIT License"],
 )
